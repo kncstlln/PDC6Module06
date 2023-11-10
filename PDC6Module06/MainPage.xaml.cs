3cb95bca7a92a707bf0665c4e4cc3204f21dd41d
kncstlln <castillano.kaneerryl@auf.edu.ph>
2023-11-10 10:33:26 +0800

    first commit

---
+++ PDC6Module06/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC6Module06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Accelerometer_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AccelerometerPage());
+        }
+
+        private async void Connectivity_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConnectivityPage());
+        }
+    }
+}
